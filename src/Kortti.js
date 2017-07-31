0b9f0d32411b4ca36bb92d97dec636ddcd281cfa
Markku Reunanen <markkureunanen@Markku-MacBook-Pro.local>
2017-07-31 18:48:29 +0300

    eka-branch
    eka-branch

---
+++ src/Kortti.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './colors.css';
+
+class Kortti extends Component {
+  render()
+  {
+    return(
+    <div>
+      <p>Haloo</p>
+    </div>
+    );
+  }
+}
+export default Kortti;
